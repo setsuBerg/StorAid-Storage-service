a801cf706241f0a06b6458b38cc7dbc8e4b9f3df
Setsuko Bergsjo <setea.music@gmail.com>
2025-10-25 11:56:13 +0200

    update Hero section

---
+++ src/components/AboutUsSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './AboutUsSection.css'
+
+const AboutUsSection = () => {
+  return (
+
+    // ------ About us section ------- //
+    <section className='aboutus-section'>
+
+
+    </section>
+
+  )
+}
+
+export default AboutUsSection
