4869351681507b58bd770907baeafca1ccddda4d
Setsuko Bergsjo <setea.music@gmail.com>
2025-11-08 16:26:32 +0100

    feat: update get-in-touch validation

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 
-import { Link } from 'react-router-dom' 
 import './subscribe.css'
 
 const Subscribe = () => {
@@ -8,46 +7,62 @@ const Subscribe = () => {
     const [submitted, setSubmitted] = useState(false)
     const [errors, setErrors] = useState({})
 
+    const validateField = (name, value) => {
+        let error = '';
+
+
+        if (name === 'email' && !/^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z0-9]{2,}$/.test(value)) {
+            error = "Must be an valid email (eg. username@example.com)."
+        }
+
+        setErrors(prev => ({...prev, [name]: error}));
+    }
+
+    const validateForm = () => {
+        const newErrors = {}
+
+        if (!/^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z0-9]{2,}$/.test(formData.email)) {
+            newErrors.email = "Must be a valid email (eg. username@example.com)"
+        }
+
+        setErrors(newErrors)
+        return Object.keys(newErrors).length === 0;
+    }
+
+
     const handleChange = (e) => {
-        const { name, value } = e.target
-        setFormData({...formData, [name]: value})
+        const { name, value } = e.target;
+        setFormData({...formData, [name]: value});
 
         if (value.trim() === '') {
-        setErrors(prevErrors => ({...prevErrors,[name]: `This ${name} field is required.`}))
-        } else {
-            setErrors(prevErrors => ({...prevErrors,[name]: '' }))
-        }
-    }   
+            setErrors(prev => ({...prev,[name]: `This ${name} field is required.`}));
+            return;
+        } 
+
+        setErrors(prev => ({...prev, [name]: '' }));
+        validateField(name, value);
+    };
+
     const handleOk = () => {
-        setSubmitted(false)
+        setSubmitted(false);
     }
 
     const handleSubmit = async (e) => {
-        e.preventDefault()
-
-        const newErrors = {}
-            Object.keys(formData).forEach(field => {
-
-                if(formData[field].trim() === ''){
-                    newErrors[field] = `This ${field} field is required.`
-                }
-            })
-            if(Object.keys(newErrors).length > 0) {
-            setErrors(newErrors)
-            return
-        }
+        e.preventDefault();
+       
+        if(!validateForm()) return;
 
         const res = await fetch('https://win25-jsf-assignment.azurewebsites.net/api/subscribe', { 
             method:'post',
             headers:{ 'Content-Type': 'application/json' },
             body: JSON.stringify(formData), 
-        })
+        });
     
         if (res.ok) { 
             setSubmitted(true)
-            setFormData({email: '' })
-        }
-    }
+            setFormData({email: '' });
+        };
+    };
 
     // if(submitted){
     //     return(
@@ -70,7 +85,7 @@ const Subscribe = () => {
                 <div className='form-group'>
                     <form onSubmit={handleSubmit} noValidate>
                         <div className='field'>
-                            <input className='email' type='email' name='email' value={formData.email} onChange={handleChange} required placeholder='Enter your email' aria-label='email' />
+                            <input className='email' type='email' name='email' value={formData.email} onChange={handleChange} required placeholder='Enter your email' aria-label='email' aria-invalid={!!errors.email} />
                             <span className='asterisk' aria-hidden>*</span>
                             <span className='error-text'>{errors.email && errors.email}</span>
                          </div>
